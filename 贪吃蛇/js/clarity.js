7571ef543a5d8c6a4fc9cf153c3d4aeca28e0158
zhyDaDa <zhyhymy040302@163.com>
2022-07-09 12:27:32 +0800

    贪吃蛇采用2维数组优化, 现在是还没成功的一代

---
+++ 贪吃蛇/js/clarity.js
@@ -0,0 +1,11 @@
+'use strict'
+
+document.getElementById("h5_1").innerHTML= 'Load Success';
+
+const clarity = function () {
+   fillStyle: function(color,loc_x,loc_y,width,height) {
+      
+   }
+}
+
+let game = new clarity();
